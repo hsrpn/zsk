92623fa3d6db1a192d40211b7c1f26ec2c09b09f
Helena Sierpińska <65978969+hsrpn@users.noreply.github.com>
2021-09-09 07:53:53 +0200

    Update 1.php

--- 1.php
+++ 1.php
@@ -3,5 +3,33 @@
   echo 'ZSK<br>';
 
   $name "Janusz";
+  echo "<br>"."twoje imię: $name"<br>;
+  //konkatenacja
+  echo 'twoje imię: $name';
+
+  //typy danych
+  $prawda = true;
+  $fałsz = false;
+  //integer
+  $całkowita = 10;
+  $bin = 0b1011;
+  $oct = 011;
+
+  $hex = 0x11;
+  echo $hex;
+  echo gettype($hex); //integer
+  echo gettype($prawda); //boolean
+  $x = 10.5;
+  echo gettype($x); //double
+  //here doc
+  $surname = "Nowak";
+  //nie nalezy umieszczac spacji w etykiecie
+  $x <<< ETYKIETA
+    <hr>
+    Anna $surname<br>
+    ETYKIETA;
+
+  echo $x;
+
 
   ?>
