1ab931bd180538236cdeb36ce2d76cb2484649d0
hsrpn <sierpinskah8@gmail.com>
2021-10-28 07:52:17 +0200

    28 10 2021

---
+++ 1_bazy.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Użytkownicy</title>
+  </head>
+  <body>
+    <h4>Użytkownicy</h4>
+    <?php
+    $connect = new mysqli("localhost","root", "", "zsk_4cg2_baza1");
+    //server, user, password, database name
+    //przechowywany jest id polaczenia
+    $sql = "SELECT * FROM `users`;";
+
+    $result = $connect->query($sql);
+    //$row = $result-> fetch_assoc();
+    // print_r($row);
+    while ($row = $result-> fetch_assoc()) {
+      //dopoki posiada rekordy
+      echo <<< ROW
+      Id: $row[id]<br>
+      Imię i nazwisko: $row[name] $row[surname]<br>
+      Data urodzenia: $row[birthday]<hr>
+      ROW;
+    }
+    $connect->close();
+     ?>
+  </body>
+</html>
